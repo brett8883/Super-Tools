74802424e0448cdeb94270c3fc8f7c9a1e859dd7
brett8883 <50305633+brett8883@users.noreply.github.com>
2019-07-06 13:39:15 -0700

    update

--- downloadtools.bat
+++ downloadtools.bat
@@ -13,16 +13,16 @@ ECHO ***************************************************************************
 ECHO *******************************************************************************************
 ECHO Hang on while I get things set up. This won't take long...
 echo.
-java -jar download.jar https://raw.githubusercontent.com/brett8883/Super-Tools/2.0/dji_verify_step.bat dji_verify_step.bat
-java -jar download.jar https://github.com/brett8883/Super-Tools/raw/2.0/ProgramFiles/NLDApp.exe NLDApp.exe
+java -jar download.jar https://raw.githubusercontent.com/brett8883/Super-Tools/master/dji_verify_step.bat dji_verify_step.bat
+java -jar download.jar https://github.com/brett8883/Super-Tools/raw/master/ProgramFiles/NLDApp.exe NLDApp.exe
 java -jar download.jar https://github.com/jezzab/DUMLdore/raw/master/DUMLdoreV3.exe DUMLdoreV3.exe
 java -jar download.jar https://github.com/jezzab/DUMLdore/raw/master/payload.bin payload.bin
 java -jar download.jar https://github.com/jezzab/DUMLdore/raw/master/payload_new.bin payload_new.bin
 java -jar download.jar https://github.com/jezzab/DUMLdore/raw/master/root.bin root.bin
 java -jar download.jar https://github.com/jezzab/DUMLdore/raw/master/root_new.bin root_new.bin
-java -jar download.jar https://github.com/brett8883/Super-Tools/2.0/master/ProgramFiles/AdbWinApi.dll AdbWinApi.dll
-java -jar download.jar https://github.com/brett8883/Super-Tools/2.0/master/ProgramFiles/AdbWinUsbApi.dll AdbWinUsbApi.dll
-java -jar download.jar https://github.com/brett8883/Super-Tools/2.0/master/ProgramFiles/adb.exe adb.exe
+java -jar download.jar https://github.com/brett8883/Super-Tools/master/master/ProgramFiles/AdbWinApi.dll AdbWinApi.dll
+java -jar download.jar https://github.com/brett8883/Super-Tools/master/master/ProgramFiles/AdbWinUsbApi.dll AdbWinUsbApi.dll
+java -jar download.jar https://github.com/brett8883/Super-Tools/master/master/ProgramFiles/adb.exe adb.exe
 java -jar download.jar https://github.com/jkson5/jkson_fcc_mod/raw/master/dos2unix.exe dos2unix.exe
 java -jar download.jar https://github.com/brett8883/Super-Tools/raw/master/jkson_verify.bat jkson_verify.bat
 java -jar download.jar https://raw.githubusercontent.com/brett8883/Super-Tools/2.0/End.bat End.bat
