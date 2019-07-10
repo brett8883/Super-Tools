0aa60ae9e932b6dee002556d5d9634432adc227a
brett8883 <50305633+brett8883@users.noreply.github.com>
2019-07-10 00:48:19 -0700

    Update whichAC.bat

--- whichAC.bat
+++ whichAC.bat
@@ -88,7 +88,7 @@ set AC=P4P
 set fc=03.02.44.09
 set stock=1.05.0600  
 set vt=2
-set fw=https://github.com/brett8883/Super-Firmware_Cache/raw/master/P4P_Super-Patcher_bin/P4P_wm331_Super_Patcher_0306_03.02.44.31_dji_system.bin
+set fw=https://github.com/brett8883/Super-Firmware_Cache/raw/master/P4P_Super-Patcher_FC/P4P_wm331_Super_Patcher_0306_03.02.44.31_dji_system.bin
 set fw2=P4P_wm331_Super_Patcher_0306_03.02.44.31_dji_system.bin
 cls
 wget %fw%
