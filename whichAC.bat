d38b248dc1ee15504562f82484c67562ef9fddf8
brett8883 <50305633+brett8883@users.noreply.github.com>
2019-07-15 15:56:14 -0600

    Added P4Pv2

--- whichAC.bat
+++ whichAC.bat
@@ -147,11 +147,11 @@ call phantom_verify.bat
 :P4Pv2
 cls 
 set AC=P4Pv2
-set fc=
-stock=
-set vt=
-set fw=
-set fw2=
+set fc=03.03.13.99
+stock=v01.00.2200
+set vt=2
+set fw=https://github.com/brett8883/Super-Firmware_Cache/raw/master/P4Pv2_FLIGHT_CONTROLLERS/p4pv2__2200_SP_standard/P4Pv2_!!v01.00.2200!!_Super_Patcher_standard_latest_fw_required_0306_03.03.13.99_wm335_dji_system.bin
+set fw2=P4Pv2_!!v01.00.2200!!_Super_Patcher_standard_latest_fw_required_0306_03.03.13.99_wm335_dji_system.bin
 cls
 wget %fw%
 copy *.bin ..
