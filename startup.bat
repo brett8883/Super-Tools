03816e1d23afe8e316f2f29f905db557696722f9
brett8883 <50305633+brett8883@users.noreply.github.com>
2019-07-23 23:08:34 -0600

    Update startup.bat

--- startup.bat
+++ startup.bat
@@ -6,5 +6,5 @@ ECHO ---------------------------------------------------------------------------
 adb kill-server
 cls
 SETLOCAL EnableDelayedExpansion
-set appver=1.7.1
+set appver=1.7.2
 call whichAC.bat
