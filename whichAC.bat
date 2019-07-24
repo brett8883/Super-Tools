8812fc43181b5c57b88c2c5d23f872465fe392e8
brett8883 <50305633+brett8883@users.noreply.github.com>
2019-07-23 18:01:00 -0600

    Update whichAC.bat

--- whichAC.bat
+++ whichAC.bat
@@ -43,8 +43,8 @@ exit
 @Echo off
 cls
 set AC=MavicPro
-set fc=03.02.44.10
-set stock=1.04.0300
+set fc=3.02.44.10
+set stock=01.04.0300
 set vt=1
 set fw=https://github.com/brett8883/Super-Firmware_Cache/raw/master/MavicPro_MPP_super_patcher_03.02.44.10_dji_system.bin
 set fw2=MavicPro_MPP_super_patcher_03.02.44.10_dji_system.bin
@@ -64,7 +64,7 @@ call mpspk_verify.bat
 @echo off
 cls
 set AC=Spark
-set fc=03.02.43.09
+set fc=3.02.43.09
 set stock=1.00.0900 
 set vt=1
 set fw=https://github.com/brett8883/Super-Firmware_Cache/raw/master/Spark_Super_Patcher_FC/Spark_Super-Patcher_306_03.02.43.09_dji_system.bin
@@ -85,7 +85,7 @@ call mpspk_verify.bat
 @echo off
 cls
 set AC=P4P
-set fc=03.02.44.31
+set fc=3.02.44.31
 set stock=1.05.0600  
 set vt=2
 set fw=https://github.com/brett8883/Super-Firmware_Cache/raw/master/P4P_Super-Patcher_FC/P4P_wm331_Super_Patcher_0306_03.02.44.31_dji_system.bin
@@ -106,8 +106,8 @@ call phantom_verify.bat
 @echo off
 cls
 set AC=P4A
-set fc=03.02.35.32
-set stock=1.00.0128
+set fc=3.02.35.32
+set stock=01.00.0128
 set vt=2
 set fw=https://github.com/brett8883/Super-Firmware_Cache/raw/master/P4Advanced_Super_Patcher_FC/P4Advanced_wm332_Super_Patcher_FC_0306_03.02.35.32_dji_system.bin
 set fw2=P4Advanced_wm332_Super_Patcher_FC_0306_03.02.35.32_dji_system.bin
@@ -127,7 +127,7 @@ call phantom_verify.bat
 @echo off
 cls
 set AC=P4Standard
-set fc=03.02.44.33
+set fc=3.02.44.33
 set stock=2.00.0700 
 set vt=2
 set fw=https://github.com/brett8883/Super-Firmware_Cache/raw/master/P4Standard_Super_Patcher_FC/P4Standard_wm330_0306_03.02.44.33_Super_Patcher_dji_system.bin
@@ -147,8 +147,8 @@ call phantom_verify.bat
 :P4Pv2
 cls 
 set AC=P4Pv2
-set fc=03.03.04.35
-stock=V01.00.1500
+set fc=3.03.04.35
+stock=1.00.1500
 set vt=2
 set fw=https://github.com/brett8883/Super-Firmware_Cache/raw/master/P4Pv2_FLIGHT_CONTROLLERS/P4PV2_Super_Patcher_FC/P4PV2_wm335_Super_Patcher_0306_03.03.04.35_dji_system.bin
 set fw2=P4PV2_wm335_Super_Patcher_0306_03.03.04.35_dji_system.bin
