80320fd86e919e7fb0807c31184ee14e03558ada
brett8883 <50305633+brett8883@users.noreply.github.com>
2019-07-27 01:27:28 -0600

    delete old

--- getFCprops.cmd
+++
@@ -1,7 +0,0 @@
-@echo off
-cd Tools 
-adb shell grep -i 'device id' /data/upgrade/backup/*.cfg.sig
-adb shell grep -i 'firmware formal' /data/upgrade/backup/*.cfg.sig
-adb shell grep -i '0306' /data/upgrade/backup/*.cfg.sig
-adb shell cd /vendor/bin/; ls
-pause
--- startup.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-adb kill-server
-cls
-SETLOCAL EnableDelayedExpansion
-set appver=2.0
-call whichAC.bat
