bcb3d76f35af77a4e5e736067845ed17bc4f7dbe
brett8883 <50305633+brett8883@users.noreply.github.com>
2019-08-07 21:21:29 -0600

    Update MainMenu.cmd

--- MainMenu.cmd
+++ MainMenu.cmd
@@ -115,7 +115,50 @@ call checkfc.bat
 goto mainmenu
 
 :flashstock 
-call flashstock.cmd
+cls
+call header.bat
+ECHO. 
+ECHO  Please choose your Aircraft
+ECHO.
+ECHO ----------------------------------------------------------------------------------------------------------------------------------------------------------------
+ECHO	1) Mavic Pro 1 / Mavic Platinum / Mavic Artic White
+ECHO	2) Spark
+ECHO	3) Phantom 4 Pro
+ECHO	4) Phantom 4 Standard
+ECHO	5) Phantom 4 Advanced
+ECHO	6) Phantom 4 Pro V2
+ECHO.
+choice /C 123456 /D 1 /T 99 /M "Please select connected device"
+If Errorlevel 6 goto P4Pv2
+If Errorlevel 5 goto p4a
+If Errorlevel 4 goto p4s
+If Errorlevel 3 goto p4p
+If Errorlevel 2 goto SPK
+If Errorlevel 1 goto MP
+
+:MP
+@echo on
+cd %sppath%
+pause
+wget http://dji.polybotes.feralhosting.com/DJI-Firmware/BIN/Mavic%20Pro%201%20-%20Incl%20Platinum%20and%20Alpine/V01.04.0300_Mavic_dji_system.bin
+set stockfw=V01.04.0300_Mavic_dji_system.bin
+pause
+cls
+goto flash
+
+:flash
+cls
+call header.bat
+echo Turn on your aircraft and connect it to your PC. When it has fully started and connected to this PC, please continue.
+pause
+cls
+call header.bat
+echo Please press "load firmware" in Dumldore and choose the %stockfw% file which is in your super patcher folder at %sppath%
+echo.
+echo Then press "flashfirmware" in Dumldore
+cd %ddpath%
+start dumldorev3.exe
+cd %stpath%
 goto mainmenu
 
 :Assistant
