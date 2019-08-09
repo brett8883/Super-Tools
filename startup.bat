76dfe4791885d158cf8287c783c0b7bf096e3678
brett8883 <50305633+brett8883@users.noreply.github.com>
2019-08-08 20:50:57 -0600

    Update startup.bat

--- startup.bat
+++ startup.bat
@@ -1,5 +1,7 @@
 @echo off
 mode con: cols=92 lines=40
+echo %date%_%time% > %sp%\logs\errorlog.txt
+echo %date%_%time% > 2> %sp%\logs\log.txt
 cls
 SETLOCAL EnableDelayedExpansion
 set appver=1.7.4
@@ -10,4 +12,6 @@ cd %st%
 cd tools
 set tools=%cd%
 cd %st%
+echo %date%_%time% > %sp%\logs\errorlog.txt
+echo %date%_%time% > %sp%\logs\log.txt
 call whichAC.bat
