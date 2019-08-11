650f7f1e26d6749ce1c30de368977e4f959396d5
brett8883 <50305633+brett8883@users.noreply.github.com>
2019-08-11 13:07:37 -0600

    updated

--- MainMenu.cmd
+++ MainMenu.cmd
@@ -21,8 +21,9 @@ echo                   [4] Launch DULMdore by Jezzeb
 echo                   [5] Launch jkson fcc mod (Mavic and P4Pv2 only)
 echo                   [6] Launch the free NoLimitDronez app
 echo.
-Choice /c 12345678 /M "Please make a section with keyboard[1,2,3,4,5,6,7,8]?"
-
+Choice /c 123456 /M "Please make a section with keyboard"
+If Errorlevel 6 goto NLD
+If Errorlevel 5 goto jkson
 If Errorlevel 4 goto dumldore
 If Errorlevel 3 goto getDownloader
 If Errorlevel 2 goto flashstock 
@@ -51,10 +52,6 @@ del /f /q *.md
 cd ..
 goto mainmenu
 
-:fcLibrary
-cls
-call fcLibrary.bat
-goto mainmenu
 
 :dumldore
 cd %ddpath%
@@ -62,113 +59,14 @@ start dumldorev3.exe || goto errorduml
 cd %stpath%
 goto mainmenu
 
-:errorduml
-cd ..
-cd ..
-cd ..
-echo ERROR_COULD_NOT_OPEN_DUMLDORE_%appver%_MPSPK >> log.txt
-echo %DATE%_%TIME% >> log.txt
-echo ERROR_COULD_NOT_OPEN_DUMLDORE_%appver%_MPSPK >> errorlog.txt
-echo %DATE%_%TIME% >> errorlog.txt
-cd Super-Tools-%branch%
-cd tools
-cls
-Echo *****************************************************************************************************
-Echo *****************************************************************************************************
-ECHO SUPER-PATCHER Error handling Wizard %appver% 
-echo By Brett8883
-Echo *****************************************************************************************************
-Echo *****************************************************************************************************
-Echo. 
-Echo ERROR
-echo.
-echo ERROR CODE M_M_COULD_NOT_OPEN_DUMLDORE
-ECHO. 
-ECHO Whoops! Something is not right. Sorry about this!
-echo. 
-echo Super-Patcher tried to open DUMLdore but couldn't.
-echo.  
-echo Sometimes this is because DUMLdore was already running in the background or 
-echo this computer is blocking it from opening.
-echo. 
-echo Check to make sure that DUMLdore is not already running by going to the task manager and end task for 
-echo DUMLdore if it is running and then continue. 
-echo. 
-echo If you are running Windows in a vertual machine then there is a good chance the VM is not set up properly
-echo. 
-echo When you are ready to try again please continue
-echo.
-pause
-goto dumldore
+
 
 :jkson
-cd tools
+cd %tpath%
 call jkson.bat
+cd %stpath%
 goto mainmenu
 
-:checkfc
-call checkfc.bat
-goto mainmenu
-
-:Assistant
-cls
-call header.bat
-ECHO I can help you install DJI Assistant 2 1.1.2 in debug mode but first you will need to uninstall any other version of Assistant 2 
-Echo.
-echo Please uninstall any other version of Assistant 2 you already have installed and then continue 
-echo. 
-echo If you do not have any version of Assistant 2 install of this machine you may continue
-pause
-cls 
-call header.bat 
-Echo Assistant 2 1.1.2 is about 120MB would you like me to download it for you?
-ECHO. 
-choice /m "Please make a selction with your keyboard 
-If Errorlevel 2 goto mainmenu
-If Errorlevel 1 goto installAssistant
-
-:installAssistant
-cls
-call header.bat
-ECHO Please wait while I download Assistant 2 1.1.2 ...
-cd tools 
-copy wget.exe Assistant_2_files
-copy 7za.exe Assistant_2_files
-cd Assistant_2_files
-wget https://www.sekidorc.com/press/DJI_Assistant2_Installer_v1.1.2_20170527.zip
-7za.exe e DJI_Assistant2_Installer_v1.1.2_20170527.zip
-del /f DJI_Assistant2_Installer_v1.1.2_20170527.zip
-cls 
-cd ..
-cd ..
-call header.bat
-cd tools 
-cd Assistant_2_files
-Echo Ok I have downloaded Assistant 2 1.1.2
-ECHO I will now open the Assistant 2 1.1.2 installer and then you'll need to complete the installation 
-echo.
-echo But we wont't be done yet at that point. We still need to set up debug mode
-echo. 
-echo Once you have installed Assistant 2 1.1.2 please check back with Super-Patcher so we csn set up debug mod
-ECHO. 
-echo. 
-ECHO Continue once ready to install Assistant 1.1.2
-echo.
-pause
-start "DJI Assistant 2 1.1.2.573 2017_05_27 16_41_02 6e0216bf(b21de8d8).exe"
-copy main.js ..
-cd ..
-copy main.js ..
-del main.js
-cd ..
-copy main.js ..
-del main.js
-cd Super-Tools-%branch%
-cls
-call header.bat
-Once you have installed Assistant 2 1.1.2
-pause
-goto mainmenu
 
 :startSp 
 cls
--- mpspk_verify.cmd
+++ mpspk_verify.cmd
@@ -1,21 +1,13 @@
 @Echo off
-title Super-Patcher 2.0 dummy_verify for %AC%
+title Super-Patcher 2.0 for %AC%
 :main
-mode con: cols=160 lines=45
 cls
-cd ..
 Echo STARTED-mpspk_for_%AC% >> log.txt
 echo %DATE%_%TIME% >> log.txt
-cd Super-Tools-%branch%
-cd tools
-set tlspath=%cd%
-echo %tlspath%
-echo %sppath%
-echo %stpath%
-pause
+cd %tpath%
 cls
 echo ----------------------------------------------------------------------------------------------------------------------------------------------------------------
-echo mpspk_verify for %AC%
+echo mpspk for %AC%
 echo ----------------------------------------------------------------------------------------------------------------------------------------------------------------
 call header.bat
 ECHO PLEASE TAKE CARE TO FOLLOW INSTRUCTIONS EXACTLY 
@@ -24,9 +16,6 @@ ECHO DO NOT SKIP STEPS, DO NOT DO EXTRA STEPS
 Echo ****************************************************************************************************************************************************************
 pause
 cls
-echo ----------------------------------------------------------------------------------------------------------------------------------------------------------------
-echo mpspk_verify for %AC%
-echo ----------------------------------------------------------------------------------------------------------------------------------------------------------------
 call header.bat
 Echo Please turn on the %AC% and after it has fully started, connect to the PC
 echo.
@@ -34,9 +23,6 @@ echo Continue when ready to begin
 Echo ****************************************************************************************************************************************************************
 Pause
 cls
-echo ----------------------------------------------------------------------------------------------------------------------------------------------------------------
-echo mpspk_verify for %AC%
-echo ----------------------------------------------------------------------------------------------------------------------------------------------------------------
 call header.bat
 Echo Please ensure DUMLdore says you are on firmware %stock% and that this is stock version 
 echo. 
@@ -46,31 +32,22 @@ echo.
 echo then close DUMLdore
 Echo. 
 Echo Continue once ADB has been enabled and DUMLdore is closed
-cd DUMLdore
-start DUMLdoreV3.exe || goto errorduml
+cd %ddpath%
+start DUMLdoreV3.exe
+cd %tpath%
 Echo ***************************************************************************************************************************************************************
 pause
-cd ..
 cls
-echo ----------------------------------------------------------------------------------------------------------------------------------------------------------------
-echo mpspk_verify for %AC% dummy_verify step
-echo ----------------------------------------------------------------------------------------------------------------------------------------------------------------
 call header.bat
-Echo Start mpspk-upload_dummy %date%_%time% >> log.txt
 Echo WORKING PLEASE WAIT... 
-adb shell mount -o remount,rw /vendor 2>> log.txt
-adb shell mkdir /vendor/bin 2>> log.txt
-adb push dummy_verify.sh /vendor/bin/ 2>> log.txt
-adb shell cd /vendor/bin/; chown root:root dummy_verify.sh; chmod 755 dummy_verify.sh; cp /sbin/dji_verify /vendor/bin/original_dji_verify_copy; sync; cd / 2>> log.txt
-adb shell mount -o remount,ro /vendor 2>> log.txt
-adb kill-server 2>> log.txt
-if %errorlevel%==0 echo dummy_verify.sh step success! && cd .. && cd .. && echo DUMMY_VERIFY_STEP_SUCCESS >> log.txt && echo %DATE%_%TIME% >> log.txt
-cd Super-Tools-%branch%
-cd tools
+adb shell mount -o remount,rw /vendor && Echo Success making vendor read and write & sleep 1
+adb shell mkdir /vendor/bin && Echo Success making directory vendor/bin & sleep 1
+adb push dummy_verify.sh /vendor/bin/ && echo Success pushing dummy_verify to %ac% & sleep 1
+adb shell cd /vendor/bin/; chown root:root dummy_verify.sh; chmod 755 dummy_verify.sh; cp /sbin/dji_verify /vendor/bin/original_dji_verify_copy; sync; cd / && echo Success activating dummy_verify.sh & sleep 1
+adb shell mount -o remount,ro /vendor && echo Success making vendor read only again. Aircraft requires reboot. Please wait to return to program
+adb kill-server 2>> errorlog.txt
+sleep 3
 cls
-echo ----------------------------------------------------------------------------------------------------------------------------------------------------------------
-echo mpspk_verify for %AC%
-echo ----------------------------------------------------------------------------------------------------------------------------------------------------------------
 call header.bat
 Echo Please restart your aircraft
 Echo.
@@ -80,36 +57,28 @@ echo Once fully restarted and connected please continue
 pause
 cls
 :bind
-echo ----------------------------------------------------------------------------------------------------------------------------------------------------------------
-echo mpspk_verify for %AC%
-echo ----------------------------------------------------------------------------------------------------------------------------------------------------------------
+call header.bat
+echo Starting bind step...
+sleep 2
+cls
 call header.bat
 echo click "Enable ADB" in DUMLdore and then close DUMLdore before proceeding
-cd dumldore
+cd %ddpath%
 start DUMLdoreV3.exe || goto errorduml
-cd ..
+cd %tpath%
 Echo ***************************************************************************************************************************************************************
 Pause
 cls
-echo ----------------------------------------------------------------------------------------------------------------------------------------------------------------
-echo mpspk_verify for %AC% bind step
-echo ----------------------------------------------------------------------------------------------------------------------------------------------------------------
 call header.bat
 ECHO WORKING. PLEASE WAIT...
-adb shell mount -o bind /vendor/bin/dummy_verify.sh /sbin/dji_verify 2>> errorlog.txt
-if %errorlevel%==0 echo %AC% BIND STEP success!
-cd ..
-cd ..
-echo %AC% BIND STEP attempted >> log.txt
-echo %DATE%_%TIME% >> log.txt
-cd Super-Tools-%branch%
-cd tools
-adb kill-server
-timeout 5
+adb shell mount -o bind /vendor/bin/dummy_verify.sh /sbin/dji_verify && echo Bind step completed successfully & sleep 2
+adb kill-server 2>> null
+del /f /s null
 cls
-echo ----------------------------------------------------------------------------------------------------------------------------------------------------------------
-echo mpspk_verify for %AC%
-echo ----------------------------------------------------------------------------------------------------------------------------------------------------------------
+call header.bat
+echo Starting flash step...
+sleep 2
+cls
 call header.bat
 echo ***DO NOT TURN OFF AIRCRAFT!***
 Echo ----------------------------------------------------------------------------------------------------------------------------------------------------------------
@@ -129,115 +98,9 @@ Echo
 Echo.
 echo 6. Once aircraft has restarted fully and connected to PC. Please continue
 echo.
-cd dumldore
-start DUMLdoreV3.exe || goto errorduml
-cd ..
-cd ..
-cd ..
-ECHO STARTED_LOAD_AND_FLASH >> log.txt
-echo %DATE%_%TIME% >> log.txt
-cd Super-Tools-%branch%
-cd tools
+cd %ddpath%
+start DUMLdoreV3.exe
+cd %tpath%
 pause
 cls 
 Call jkson_verify.bat
-
-:error2
-cls
-adb kill-server
-cd ..
-cd ..
-cls
-mode con: cols=107 lines=45
-Echo *****************************************************************************************************
-Echo *****************************************************************************************************
-ECHO SUPER-PATCHER Error handling Wizard %appver% 
-echo By Brett8883
-Echo *****************************************************************************************************
-Echo *****************************************************************************************************
-echo ERROR_CODE_MSPK-2_UNABLE_TO_REACH_AIRCRAFT_VIA_AD_AT_BIND_STEP_%appver% >> log.txt
-echo %DATE%_%TIME% >> log.txt
-echo ERROR_CODE_MSPK-2_UNABLE_TO_REACH_AIRCRAFT_VIA_AD_AT_BIND_STEP_%appver% >> errorlog.txt
-echo %DATE%_%TIME% >> errorlog.txt
-echo ERROR CODE MSPK-2 UNABLE TO REACH AIRCRAFT VIA ADB AT BIND STEP
-echo.
-echo HEY! WOAH! Sorry! There was an issue with that last step. 
-echo. 
-echo Best I can tell the aircraft is either not connected to this PC or 
-echo that "Enable ADB" with DUMLdore didn't get done right
-echo. 
-ECHO Make sure the aircraft is connected and turned on then continue
-echo I'll take you back to DUMLdore and we'll give it another go 
-pause
-cd Super-Tools-%branch%
-cd tools
-goto bind 
-
-:error1
-adb kill-server
-mode con: cols=107 lines=45
-cls
-cd ..
-cd ..
-Echo *****************************************************************************************************
-Echo *****************************************************************************************************
-ECHO SUPER-PATCHER Error handling Wizard %appver% 
-echo By Brett8883
-Echo *****************************************************************************************************
-Echo *****************************************************************************************************
-echo ERROR_CODE_MSPK-1_UNABLE_TO_REACH_AIRCRAFT_VIA_AD_AT_DUMMY_STEP_%appver% >> log.txt
-echo %DATE%_%TIME% >> log.txt
-echo ERROR_CODE_MSPK-1_UNABLE_TO_REACH_AIRCRAFT_VIA_AD_AT_DUMMY_STEP_%appver% >> errorlog.txt
-echo %DATE%_%TIME% >> errorlog.txt
-Echo ERROR CODE MSPK-1 UNABLE TO REACH %AC% VIA ADB at upload dummy_verify Step
-echo.
-echo HEY! WOAH! Sorry! There was an issue with that last step. 
-echo. 
-echo Best I can tell the aircraft is either not connected to this PC or 
-echo that "Enable ADB" with DUMLdore didn't get done right
-echo. 
-ECHO Make sure the aircraft is connected and turned on and then continue and 
-echo I'll take you back to DUMLdore and we'll give it another go 
-cd Super-Tools-%branch%
-cd tools
-echo.
-pause
-goto main 
-
-:errorduml
-mode con: cols=107 lines=45
-cd ..
-cd ..
-echo ERROR_COULD_NOT_OPEN_DUMLDORE_%appver%_MPSPK >> log.txt
-echo %DATE%_%TIME% >> log.txt
-echo ERROR_COULD_NOT_OPEN_DUMLDORE_%appver%_MPSPK >> errorlog.txt
-echo %DATE%_%TIME% >> errorlog.txt
-cd Super-Tools-%branch%
-cd tools
-cls
-Echo *****************************************************************************************************
-Echo *****************************************************************************************************
-ECHO SUPER-PATCHER Error handling Wizard %appver% 
-echo By Brett8883
-Echo *****************************************************************************************************
-Echo *****************************************************************************************************
-Echo. 
-Echo ERROR ERROR ERROR
-echo.
-echo ERROR CODE MPSPK_VRFY_%AC%_COULD_NOT_OPEN_DUMLDORE
-ECHO. 
-ECHO Whoops! Something is not right. Sorry about this!
-echo. 
-echo Super-Patcher tried to open DUMLdore but couldn't.
-echo.  
-echo Sometimes this is because DUMLdore was already running in the background or 
-echo this computer is blocking it from opening.
-echo. 
-echo Check to make sure that DUMLdore is not already running by going to the task manager and end task for 
-echo DUMLdore if it is running and then continue. 
-echo. 
-echo If you are running Windows in a vertual machine then there is a good chance the VM is not set up properly
-echo. 
-echo When you think you have solved the problem please restart the Super-Patcher process
-pause
-exit
