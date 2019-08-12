c465d465e7bf3b0a8b5d10ebd8d3d31df439f156
brett8883 <50305633+brett8883@users.noreply.github.com>
2019-08-12 00:09:21 -0600

    Update variantchooser.cmd

--- variantchooser.cmd
+++ variantchooser.cmd
@@ -14,10 +14,16 @@ ECHO.
 choice
 if errorlevel 2 set stealthmod=0
 if errorlevel 1 set stealthmod=1
-set variant=%batmod%%stealthmod%
-if "%variant%"=="00" echo Standard
-if "%variant%"=="10" echo batmod
-if "%variant%"=="01" echo stealthmod
-if "%variant%"=="11" echo loaded
+set varchoice=%batmod%%stealthmod%
+if "%varchoice%"=="00" set variant=Standard 
+if "%varchoice%"=="10" set variant=battmod
+if "%varchoice%"=="01" set variant=stealth
+if "%varchoice%"=="11" set variant=loaded
+set fw=%AC%_SP_2.0_
+set fwcache=https://github.com/brett8883/Super-Firmware_Cache/raw/
+set fwcachebranch=/2.0/
+rem https://github.com/brett8883/Super-Firmware_Cache/raw/2.0/MavicPro/2.0/MavicPro_SP_2.0_standard/MavicPro_SP_2.0_03.02.44.11_dji_system.bin
+set finalurl=%fwcache%%fwcachebranch%%AC%%fwcachebranch%%AC%_SP_2.0_%variant%/%AC%_SP_2.0_%fc%_dji_system.bin
+echo %finalurl%
+wget %finalurl%
 pause
-
