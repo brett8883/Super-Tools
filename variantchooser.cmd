3f5b2d41fc46b70a9ec31f8df19b93d8563ab33d
brett8883 <50305633+brett8883@users.noreply.github.com>
2019-08-11 23:41:15 -0600

    Create variantchooser.cmd

---
+++ variantchooser.cmd
@@ -0,0 +1,22 @@
+@echo off
+cls
+call header.bat
+ECHO Would you like to enable the new smart battery mod?
+echo.
+choice 
+if errorlevel 2 set batmod=1
+if errorlevel 1 set batmod=0
+cls
+call header.bat
+echo Would you like to enable the stealth mod?
+ECHO.
+choice
+if errorlevel 2 set stealthmod=1
+if errorlevel 1 set stealthmod=0
+set variant=%batmod%%stealthmod%
+if "%variant%"=="00" echo Standard
+if "%variant%"=="10" echo batmod
+if "%variant%"=="01" echo stealthmod
+if "%variant%"=="11" echo loaded
+pause
+
