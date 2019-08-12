83cca92aea155b7b808da1aa9a8d17cb36adf8f4
brett8883 <50305633+brett8883@users.noreply.github.com>
2019-08-12 17:05:08 -0600

    Update variantchooser.cmd

--- variantchooser.cmd
+++ variantchooser.cmd
@@ -15,10 +15,10 @@ choice
 if errorlevel 2 set stealthmod=1
 if errorlevel 1 set stealthmod=2
 set varchoice=%batmod%%stealthmod%
-if "%varchoice%"=="11" set variant=%standard%
-if "%varchoice%"=="21" set variant=%battmod%
-if "%varchoice%"=="12" set variant=%stealth%
-if "%varchoice%"=="22" set variant=%battmodstealth%
+if "%varchoice%"=="11" set variant=standard
+if "%varchoice%"=="21" set variant=battmod
+if "%varchoice%"=="12" set variant=stealth
+if "%varchoice%"=="22" set variant=battmodstealth
 set fw2=%AC%_SP_2.0_%variant%_%fc%_dji_system.bin
 set fc=%fcbase%%varchoice%
 pause
