b54680f44d07002f516b2a4f782b5fd460484a00
brett8883 <50305633+brett8883@users.noreply.github.com>
2019-08-13 00:44:13 -0600

    Update flashstock.cmd

--- flashstock.cmd
+++ flashstock.cmd
@@ -1,6 +1,6 @@
 @echo off
 cls
-call header.bat
+call %header%
 cd %sppath%
 if exist %stockfw% (goto flash) else (goto download)
 
@@ -9,33 +9,29 @@ cd %stpath%
 echo Please Wait while I download %stockfw%. 
 Echo.
 echo When it's ready it will be in %sppath%
-wget %stockdl%
+%busybox% wget %stockdl%
 copy %stockfw% ..
 del %stockfw%
-cd ..
 goto flash
 
 :flash
-cd %stpath%
 cls
-call header.bat
+call %header%
 echo Turn on your aircraft and connect it to your PC. When it has fully started and connected to this PC, please continue.
 pause
 cls
-call header.bat
+call %header%
 echo Please press "load firmware" in Dumldore and choose the %stockfw% file
 echo. 
-echo which is in your super patcher folder at %sppath%
+echo which is in your Super-Patcher folder at %sppath%
 echo.
 echo Then press "flashfirmware" in Dumldore
 ECHO.
 ECHO.
 ECHO Once Dumldore says its ok to reboot, please reboot the aircraft and then continue to be taken back to the Main Menu
 ECHO.
-cd %ddpath%
-start dumldorev3.exe
+start %dumldore%
 pause
-cd %stpath%
 call mainmmenu.cmd
 
 
