6d4f8d4ff954233f36292d7d2fbc91b2d7b83d81
brett8883 <50305633+brett8883@users.noreply.github.com>
2019-08-15 21:11:07 -0600

    Create theGoldenOne.bat

---
+++ theGoldenOne.bat
@@ -0,0 +1,28 @@
+@echo off
+SETLOCAL ENABLEDELAYEDEXPANSION
+adb shell grep -i 'device id' /data/upgrade/backup/*.cfg.sig > devicetmp.txt
+for /f "tokens=1-3 delims==>" %%A in (devicetmp.txt) do (set device=%%B)
+adb shell grep -i 'firmware formal' /data/upgrade/backup/*.cfg.sig > firmwaretmp.txt
+for /f "tokens=1-2 delims==>" %%A in (firmwaretmp.txt) do (set cfirmware=%%B)
+adb shell grep -i '0306' /data/upgrade/backup/*.cfg.sig > FCtmp.txt
+for /f "tokens=4-5 delims== " %%A in (fctmp.txt) do (set curFC=%%B)
+  :: Remove quotes
+   SET device=###%device%###
+   SET device=%device:"###=%
+   SET device=%device:###"=%
+   SET device=%device:###=%
+echo %device%
+ :: Remove quotes
+   SET cfirmware=###%cfirmware%###
+   SET cfirmware=%cfirmware:"###=%
+   SET cfirmware=%cfirmware:###"=%
+   SET cfirmware=%cfirmware:###=%
+echo %cfirmware%
+:: Remove quotes
+   SET curFC=###%curFC%###
+   SET curFC=%curFC:"###=%
+   SET curFC=%curFC:###"=%
+   SET curFC=%curFC:###=%
+echo %curFC%
+del /f /q *tmp.txt
+pause
