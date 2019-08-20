4e1d819cafacedf3a9bf59998ec03fe9bbe6a41f
brett8883 <50305633+brett8883@users.noreply.github.com>
2019-08-19 18:59:06 -0600

    Update startup.bat

--- startup.bat
+++ startup.bat
@@ -2,5 +2,34 @@
 mode con: cols=92 lines=40
 cls
 SETLOCAL EnableDelayedExpansion
-set appver=1.7.5
+set appver=1.7.6
+SET count=1
+FOR /F "tokens=* USEBACKQ" %%F IN (`wmic os get version`) DO (
+  SET var!count!=%%F
+  SET /a count=!count!+1
+)
+set _string=%var2%
+for /f "tokens=1 delims=." %%a in ("%_string%") do (
+  set /a winver=%%a
+  )
+if "%winver%"=="10" (goto windows10) ELSE (goto windowsver)
+
+:windows10
+::Windows version is 10 notice
+Echo Windows 10 is detected
+PING -n 3 127.0.0.1>nul
+goto end
+
+:windowsver
+::not windows 10 notice
+Echo I have detected that your Windows version is Windows %winver% ...
+echo.
+echo Super-Patcher works best with Windows 10.
+echo.
+echo If you have any issues running Super-Patcher please try again on a Windows 10 PC.
+echo.
+ECHO Please continue when ready
+echo
+pause
+:end
 call whichAC.bat
