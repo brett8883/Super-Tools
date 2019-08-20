de07966e80d602801d1f507f7c3f61a7ba72ea8b
brett8883 <50305633+brett8883@users.noreply.github.com>
2019-08-20 15:07:28 -0600

    Update startup.cmd

--- startup.cmd
+++ startup.cmd
@@ -1,25 +1,25 @@
 @echo off
-rem set envirnment variables and paths
+::set envirnment variables and paths
 title Super-Patcher 2.0
 mode con: cols=160 lines=45
 set appver=2.0
-set stpath="%cd%"
-set header=%stpath%\header.bat
+set stpath=%sppath%\Super-tools-%branch%
+set header=%sppath%\Super-tools-%branch%\header.bat
 call %header%
-set mainmenu=%stpath%\MainMenu.cmd
+set mainmenu=%sppath%\Super-tools-%branch%\MainMenu.cmd
 echo Hang on one more sec...
 cd tools
-set tpath=%cd%
-set jksonbat=%tpath%\jkson.bat
-set jksonrdme=%tpath%\jkson_README.md
-set adb=%tpath%\adb.exe
+set tpath=%sppath%\Super-tools-%branch%\tools
+set jksonbat=%sppath%\Super-tools-%branch%\tools\jkson.bat
+set adb=%sppath%\Super-tools-%branch%\tools\adb.exe
+cd tools
 %busybox% wget https://github.com/jezzab/DUMLdore/archive/master.zip
 %busybox% unzip -o master.zip
 del DUMLdore-*
 del master.zip
 cd DUMLdore-master
-set ddpath="%cd%"
-set dumldore=%tpath%\DUMLdore-master\dumldorev3.exe
+set ddpath=%sppath%\Super-tools-%branch%\tools\DUMLdore-master\
+set dumldore=%sppath%\Super-tools-%branch%\tools\DUMLdore-master\dumldorev3.exe
 cd %tpath%
 wget https://nolimitdronez.com/downloads/nldapp.zip && echo Downloaded NLD successfully >> %log% & echo Downloaded NLD successfully || echo ERROR could not download NLD & echo ERROR could not download NLD >> %log% 
 %busybox% unzip -o -q nldapp.zip -d %tpath% && echo NLD unzipped successfully & echo NLD unzipped successfully >> %log% 
@@ -29,7 +29,7 @@ del /f /q *.exe
 cd %tpath%
 rmdir /Q /S NLDModClient
 del /f /q nldapp.zip
-set NLD=%tpath%\NLDApp.exe
+set NLD=%sppath%\Super-tools-%branch%\tools\nldapp.exe
 cd %stpath%
 goto skip
 
