d72a64e35d586b5968732ecf3582353a19d01a58
brett8883 <50305633+brett8883@users.noreply.github.com>
2019-08-19 21:05:58 -0600

    Update mpspk_verify.bat

--- mpspk_verify.bat
+++ mpspk_verify.bat
@@ -6,8 +6,8 @@ ECHO ---------------------------------------------------------------------------
 ECHO Super Patcher %appver%
 ECHO By Brett8883
 ECHO -------------------------------------------------------------------------------------------
-ECHO PLEASE TAKE CARE TO FOLLOW INSTRUCTIONS EXACTLY 
-ECHO GO SLOW AND READ INSTRUCTIONS CAREFULLY BEFORE ACTING 
+ECHO PLEASE TAKE CARE TO FOLLOW INSTRUCTIONS EXACTLY
+ECHO GO SLOW AND READ INSTRUCTIONS CAREFULLY BEFORE ACTING
 ECHO DO NOT SKIP STEPS, DO NOT DO EXTRA STEPS
 Echo *******************************************************************************************
 pause
@@ -25,13 +25,13 @@ ECHO ---------------------------------------------------------------------------
 ECHO Super Patcher %appver%
 ECHO By Brett8883
 ECHO -------------------------------------------------------------------------------------------
-Echo Please ensure DUMLdore says you are on firmware %stock% and that this is stock version 
-echo. 
-echo Then click "Enable ADB" in DUMLdore and wait for ADB to enable 
+Echo Please ensure DUMLdore says you are on firmware %stock% and that this is stock version
+echo.
+echo Then click "Enable ADB" in DUMLdore and wait for ADB to enable
 Echo The front lights will usually flash, turn on, or turn off
 echo.
 echo then close DUMLdore
-Echo. 
+Echo.
 Echo Continue once ADB has been enabled and DUMLdore is closed
 start DUMLdoreV3.exe
 Echo *******************************************************************************************
@@ -41,13 +41,25 @@ ECHO ---------------------------------------------------------------------------
 ECHO Super Patcher %appver%
 ECHO By Brett8883
 ECHO -------------------------------------------------------------------------------------------
-Echo WORKING PLEASE WAIT... 
-adb shell mount -o remount,rw /vendor 2>> errorlog.txt
-adb shell mkdir /vendor/bin 2>> errorlog.txt
-adb push dummy_verify.sh /vendor/bin/ 2>> errorlog.txt
-adb shell cd /vendor/bin/; chown root:root dummy_verify.sh; chmod 755 dummy_verify.sh; cp /sbin/dji_verify /vendor/bin/original_dji_verify_copy; sync; cd / 2>> errorlog.txt
-adb shell mount -o remount,ro /vendor 2>> errorlog.txt
-adb kill-server
+Echo WORKING PLEASE WAIT...
+@echo ON
+adb shell mount -o remount,rw /vendor 2>> errorlog1.txt
+adb shell mkdir /vendor/bin 2>> errorlog2.txt
+adb push dummy_verify.sh /vendor/bin/ 2>> errorlog3.txt
+adb shell cd /vendor/bin/; chown root:root dummy_verify.sh; chmod 755 dummy_verify.sh; cp /sbin/dji_verify /vendor/bin/original_dji_verify_copy; sync; cd / 2>> errorlog4.txt
+adb shell mount -o remount,ro /vendor 2>> errorlog5.txt
+@echo OFF
+echo DUMMY_VERIFY STEPS >> errorlog.txt
+echo Command:adb shell mount -o remount,rw /vendor >> errorlog.txt
+type errorlog1.txt >>errorlog.txt
+echo Command:adb shell mkdir /vendor/bin >> errorlog.txt
+type errorlog2.txt >> errorlog.txt
+type errorlog3.txt >> errorlog.txt
+type errorlog4.txt >> errorlog.txt
+set adberrorlog1=<errorlog1
+echo %adberrorlog1%
+pause
+adb kill-server 2>>nul
 cls
 ECHO -------------------------------------------------------------------------------------------
 ECHO Super Patcher %appver%
@@ -73,7 +85,8 @@ ECHO Super Patcher %appver%
 ECHO By Brett8883
 ECHO -------------------------------------------------------------------------------------------
 ECHO WORKING. PLEASE WAIT...
-adb shell mount -o bind /vendor/bin/dummy_verify.sh /sbin/dji_verify 2>> errorlog.txt
+adb shell mount -o bind /vendor/bin/dummy_verify.sh /sbin/dji_verify 2>> errorlog5.txt
+type errorlog5.txt >> errorlog.txt
 adb kill-server
 cls
 ECHO -------------------------------------------------------------------------------------------
@@ -82,23 +95,23 @@ ECHO By Brett8883
 ECHO -------------------------------------------------------------------------------------------
 echo DO NOT TURN OFF AIRCRAFT!
 Echo *******************************************************************************************
-echo 1. Please click "Load Firmware" button in DUMLdore 
-echo. 
+echo 1. Please click "Load Firmware" button in DUMLdore
+echo.
 echo 2. Choose the file %fw2%
-echo.  
+echo.
 ECHO 3. Then click "Flash Firmware" in DUMLdore
 echo.
 ECHO -------------------------------------------------------------------------------------------
-ECHO 4.	Please allow DUMLdore to flash to the aircraft 
+ECHO 4.	Please allow DUMLdore to flash to the aircraft
 echo	Do not disconnect the %AC% until DUMLdore says it is ok to
 ECHO -------------------------------------------------------------------------------------------
-echo. 
+echo.
 echo 5. Once DUMLdore is finished please restart the aircraft
 Echo.
 echo 6. Once aircraft has restarted fully and connected to PC. Please continue
-echo. 
+echo.
 start DUMLdoreV3.exe
 ECHO -------------------------------------------------------------------------------------------
 pause
-cls 
+cls
 Call jkson_verify.bat
