f9bda53510459448b1f1bf766005b018b8acd530
brett8883 <50305633+brett8883@users.noreply.github.com>
2019-10-25 17:14:25 -0500

    Revert "fix paths"
    This reverts commit 609e97ee03ae1e7147a5a524d702da566829ca7c.

--- startup.cmd
+++ startup.cmd
@@ -3,20 +3,19 @@
 title Super-Patcher 2.0
 mode con: cols=160 lines=45
 set appver=2.0
-set stpath="%Appdata%\DJI_super-Patcher\Super-tools-%branch%"
-set header="%Appdata%\DJI_super-Patcher\Super-tools-%branch%\header.bat"
+set stpath=%Appdata%\DJI_super-Patcher\Super-tools-%branch%
+set header=%Appdata%\DJI_super-Patcher\Super-tools-%branch%\header.bat
 call %header%
 echo Hang on one more sec...
 cd tools
-set tpath="%Appdata%\DJI_super-Patcher\Super-tools-%branch%\tools"
-set PATH=%path%;%tpath%
+set tpath=%Appdata%\DJI_super-Patcher\Super-tools-%branch%\tools
 %busybox% wget -q https://github.com/jezzab/DUMLdore/archive/master.zip
 %busybox% unzip -o -q master.zip
 del DUMLdore-*
 del master.zip
 cd DUMLdore-master
 set ddpath=%Appdata%\DJI_super-Patcher\Super-tools-%branch%\tools\dumldore-master
-set dumldore=dumldorev3.exe
+set dumldore=%Appdata%\DJI_super-Patcher\Super-tools-%branch%\tools\dumldore-master\dumldorev3.exe
 cd %tpath%
 Echo. >> %log%
 Echo Download NLD app >> %log%
