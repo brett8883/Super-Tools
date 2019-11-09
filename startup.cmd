193791a13e88b6aeca02a3fa7eaa2a1808e3380e
brett8883 <50305633+brett8883@users.noreply.github.com>
2019-11-08 20:54:13 -0600

    update link

--- startup.cmd
+++ startup.cmd
@@ -3,14 +3,14 @@
 title Super-Patcher 2.0
 mode con: cols=160 lines=45
 set appver=2.0
-set stpath="%Appdata%\DJI_super-Patcher\Super-tools-%branch%"
-set header="%Appdata%\DJI_super-Patcher\Super-tools-%branch%\header.bat"
+set stpath="%cd%"
+set header="%cd%\header.bat"
 call %header%
 echo Downloading the latest version of DUMLdore. Please wait...
 cd tools
-set tpath="%Appdata%\DJI_super-Patcher\Super-tools-%branch%\tools"
+set tpath="%cd%"
 set PATH=%path%;%tpath%
 wget https://github.com/jezzab/DUMLdore/raw/master/DUMLdoreV3.exe && echo downloaded DUMLdore sucessfully >> %log%
-set dumldore="%Appdata%\DJI_super-Patcher\Super-tools-%branch%\tools\DUMLdoreV3.exe"
+set dumldore="%cd%\DUMLdoreV3.exe"
 Echo. >> %log%
 call whichac.cmd
